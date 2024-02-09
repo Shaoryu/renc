a6d80b16206acd4858c65fdab5075e67b4b48c10
Shaoryu <149553583+Shaoryu@users.noreply.github.com>
2024-02-09 22:02:48 +0900

    Update renc.cpp

--- renc.cpp
+++ renc.cpp
@@ -5,22 +5,19 @@ renc::renc(InterruptIn &A,InterruptIn &B):_A(A),_B(B){}
 void renc::a_slit(){
     if(_A != _B)passed_slit++;
     else passed_slit--;
-    return 0;
 }
 
 void renc::b_slit(){
     if(_A == _B)passed_slit++;
     else passed_slit--;
-    return 0;
 }
 
 void renc::getangle(){
-        _A.rise(a_slit);
-        _A.fall(a_slit);
-        _B.rise(b_slit);
-        _B.fall(b_slit);
+        _A.rise(renc::a_slit());
+        _A.fall(renc::a_slit());
+        _B.rise(renc::b_slit());
+        _B.fall(renc::b_slit());
         _ang = 0.45f * passed_slit;
-        return 0;
 }
 
 int renc::getrev(){
@@ -30,7 +27,7 @@ int renc::getrev(){
 
 void renc::renc_read(){
     while(true){//処理はここに
-        renc.getangle();
+        renc::getangle();
         //_angが返ってきてるはず
     }
 }
