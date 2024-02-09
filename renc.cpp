ab5810b41d00da37a98314d18d014889367a5b49
Shaoryu <149553583+Shaoryu@users.noreply.github.com>
2024-02-09 21:50:09 +0900

    Create renc.cpp

---
+++ renc.cpp
@@ -0,0 +1,36 @@
+#include "renc.h"
+#include "mbed.h"
+renc::renc(InterruptIn &A,InterruptIn &B):_A(A),_B(B){}
+
+void renc::a_slit(){
+    if(a != b)passed_slit++;
+    else passed_slit--;
+    return 0;
+}
+
+void renc::b_slit(){
+    if(a == b)passed_slit++;
+    else passed_slit--;
+    return 0;
+}
+
+void renc::getangle(){
+        _A.rise(a_slit);
+        _A.fall(a_slit);
+        _B.rise(b_slit);
+        _B.fall(b_slit);
+        _ang = 0.45f * passed_slit;
+        return 0;
+}
+
+int renc::getrev(){
+    _rev = (int)_ang / 360;
+    return _rev;
+}
+
+void renc::renc_read(){
+    while(true){//処理はここに
+        renc.getangle();
+        //_angが返ってきてるはず
+    }
+}
