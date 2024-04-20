5c4e264bd36e108b685010b994b41128ed7f40eb
Shaoryu <149553583+Shaoryu@users.noreply.github.com>
2024-04-20 20:17:29 +0900

    Update renc.cpp

--- renc.cpp
+++ renc.cpp
@@ -1,25 +1,36 @@
-#ifndef Rotary_encoder_H
-#define Rotary_encoder_H
-
+#include "renc.h"
 #include "mbed.h"
 
-class renc
-{
-    private:
+renc::renc(PinName A,PinName B):_A(A),_B(B){
+}
 
-    float _ang;
-    int _rev;
-    int passed_slit;
-    
-    InterruptIn _A;
-    InterruptIn _B;
 
-    public:
-        renc(PinName A,PinName B);
-        void a_slit();
-        void b_slit();
-        void getangle();
-        int getrev();
-        void renc_read();
-};
-#endif
+void renc::a_slit(){
+    if(_A != _B)passed_slit++;
+    else passed_slit--;
+}
+
+void renc::b_slit(){
+    if(_A == _B)passed_slit++;
+    else passed_slit--;
+}
+
+void renc::getangle(){
+     _A.rise(callback(this,&renc::a_slit));
+     _A.fall(callback(this,&renc::a_slit));
+     _B.rise(callback(this,&renc::b_slit));
+     _B.fall(callback(this,&renc::b_slit));
+    _ang = 0.45f * passed_slit;
+}
+
+int renc::getrev(){
+    _rev = (int)_ang / 360;
+    return _rev;
+}
+
+void renc::renc_read(){
+    while(true){//処理はここに
+        renc::getangle();
+        //_angが返ってきてるはず
+    }
+}
